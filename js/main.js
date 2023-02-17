f90db8b405755ffaed2821f9991380b48cdd77e6
casio25 <mishakolomiets355@gmail.com>
2023-02-17 20:02:25 +0200

    added input

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 console.log("Hello World!");
+const inputField = document.querySelector(".input_field");
+const searchButton = inputField.querySelector("#search_button");
+function searchWeather(){
+    alert('Check')
+}
+searchButton.addEventListener('click', searchWeather);
