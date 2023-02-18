aff5789f7ec9c0a96bd1f4eb0e5052de18244554
casio25 <mishakolomiets355@gmail.com>
2023-02-18 17:04:15 +0200

    added simple logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 console.log("Hello World!");
-const inputField = document.querySelector(".input_field");
-const searchButton = inputField.querySelector("#search_button");
+const inputForm = document.querySelector(".input_form");
+const inputField = inputForm.querySelector("#city_input");
+const searchButton = inputForm.querySelector("#search_button");
+
+let weather = {
+    "apiKey": "6a2460f385d8fbe77660e390eb988c73"
+}
+/*Making fetch function*/
+function fetchWeather(city) {
+    return fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6a2460f385d8fbe77660e390eb988c73`
+    ).then((response) => { 
+            return response.json().then((data) => {
+                console.log(data);
+                return data;
+            }).catch((err) => {
+                console.log(err);
+            }) 
+        });
+}
 function searchWeather(){
-    alert('Check')
+    let inputValue = document.querySelector("#city_input").value;
+    fetchWeather(inputValue);
+    console.log(inputValue);
 }
 searchButton.addEventListener('click', searchWeather);
